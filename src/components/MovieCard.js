dbea464dca58c08672d78cdaf05dc605fd7f09ef
Arya Dev <aryadev1810@gmail.com>
2025-08-26 21:59:43 +0530

    Enhanced MovieCard hover animation for better UI

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -3,9 +3,21 @@ import { IMG_CDN_URL } from "../utils/constants";
 const MovieCard = ({ posterPath }) => {
   if (!posterPath) return null;
   return (
-    <div className="w-36 md:w-48 pr-4">
-      <img alt="Movie Card" src={IMG_CDN_URL + posterPath} />
-    </div>
+    // <div className="w-36 md:w-48 pr-4"> 
+    // <img alt="Movie Card" src={IMG_CDN_URL + posterPath} /> 
+    // </div>
+
+
+ <div className="w-36 md:w-48 pr-4 transform transition-transform duration-500 ease-out hover:-translate-y-2 hover:rotate-1 sm:hover:scale-105 md:hover:scale-110 hover:shadow-2xl">
+  <img
+    alt="Movie Card"
+    src={IMG_CDN_URL + posterPath}
+    className="rounded-xl hover:brightness-110"
+  />
+</div> 
+
+
+
   );
 };
 export default MovieCard;
